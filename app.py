70343dbd2a6130e98d7195ec62741e25fa8d2323
NarasimhaPatro <143970060+NarasimhaPatro@users.noreply.github.com>
2026-02-07 15:04:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-## flask app for hello world
+## flask app for hello world Code
 
 from flask import Flask
 import numpy as np
@@ -13,4 +13,5 @@ def home():
 
 
 if __name__=="__main__":
+
     app.run(host="0.0.0.0",port=5000)
